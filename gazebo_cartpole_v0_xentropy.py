3628c367351c0994402fb25a893778f527bf8eca
Bowen Yuan <180382414+byuantl@users.noreply.github.com>
2025-11-04 15:13:26 -0800

    edit: implemented cross entropy deep RL TODOs?

--- gazebo_cartpole_v0_xentropy.py
+++ gazebo_cartpole_v0_xentropy.py
@@ -202,8 +202,8 @@ def filter_batch(batch, percentile):
 if __name__ == '__main__':
     # Setup environment
     env = gym.make('GazeboCartPole-v0')
-    obs_size = #TODO: Set observation size space
-    n_actions = #TODO: Set action size space
+    obs_size = 4 #TODO: Set observation size space
+    n_actions = 2 #TODO: Set action size space
 
     # Create the NN object
     net = Net(obs_size, HIDDEN_SIZE, n_actions)
@@ -218,23 +218,23 @@ if __name__ == '__main__':
     # episodes in the top 30% and we train our NN on them.
     for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
         # Identify the episodes that are in the top PERCENTILE of the batch
-        obs_v, acts_v, reward_b, reward_m = #TODO identify the episode in top PERCENTILE
+        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE) #TODO identify the episode in top PERCENTILE
 
         # Prepare for training the NN by zeroing the acumulated gradients.
-        # TODO: zero gradients
+        optimizer.zero_grad() # TODO: zero gradients
 
         # Calculate the predicted probabilities for each action in the best 
         # episodes
-        action_scores_v = #TODO forward pass on each observation
+        action_scores_v = net(obs_v) #TODO forward pass on each observation
 
         # Calculate the cross entropy loss between the predicted actions and 
         # the actual actions
-        loss_v = #TODO calculate loss
+        loss_v = objective(action_scores_v, acts_v) #TODO calculate loss
 
         # Train the NN: calculate the gradients using and then adjust the 
         # weights based on the gradients
-        #TODO backward pass
-        #TODO adjust gradients
+        loss_v.backward() #TODO backward pass
+        optimizer.step() #TODO adjust gradients
 
         # Display summary of current batch
         print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (
